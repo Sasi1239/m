d0b25f32e8e69e3d854f0326f21e953709e56fd2
sasidharan <sasidharan2531@gmail.com>
2022-10-19 09:54:55 +0530

    intial commit

---
+++ teacher.java
@@ -0,0 +1,12 @@
+class teacher{
+        String name;
+        int Tid;
+        /*public teacher(string N,int id)
+{
+        name=N;
+        Tid=id;
+        }*/
+       void teaching(){
+        System.out.println("teaching");
+}
+}
